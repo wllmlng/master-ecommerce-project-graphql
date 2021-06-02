dc53d6e52eb311e11b240471c1483b94385c4b06
William Leung <w.leung.93@gmail.com>
2021-06-01 19:30:15 -0700

    render product page using graphql/apollo

--- src/index.js
+++ src/index.js
@@ -3,19 +3,58 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+//allows app to access apollo State
+import { ApolloProvider } from 'react-apollo';
+//let us connect our client to spacific end point (/graphql)
+import { createHttpLink } from 'apollo-link-http';
+//caches pages you've been to to optimize
+import { InMemoryCache } from 'apollo-cache-inmemory';
+//bundle of smaller library to make apollo easier for us
+import { ApolloClient, gql } from 'apollo-boost';
 
 import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 
+const httpLink = createHttpLink({
+  uri: 'https://crwn-clothing.com'
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+client.query({
+  query: gql`
+    {
+      getCollectionsByTitle(title: "hats"){
+        id
+        title
+        items {
+          id
+          name
+          price
+          imageUrl
+        }
+      }
+    }
+  `
+}).then(res => console.log(res));
+
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
